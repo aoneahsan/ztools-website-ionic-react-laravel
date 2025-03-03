d6a052b6106af675e651d988b6efd87951360d72
Ahsan Mahmood <aoneahsan@gmail.com>
2025-03-03 09:43:59 +0500

    android and ios folder added, next need to resolve build command errors

---
+++ android/app/src/main/java/com/zaions/ztools/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zaions.ztools;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/components/PublicRoute/index.tsx
+++ src/components/PublicRoute/index.tsx
@@ -6,35 +6,35 @@ import { ZRoutesRedirects } from '@/utils/constants/RoutesConstants';
 import { IsAuthenticatedRStateSelector } from '@/ZaionsStore/UserAccount/index.recoil';
 
 const PublicRoute = ({ Component, restricted = true, ...rest }: any) => {
-	return (
-		// restricted = false meaning public route
-		// restricted = true meaning restricted route
-		<Suspense fallback={<ZFallbackIonSpinner />}>
-			<PublicRouteAsync
-				Component={Component}
-				restricted={restricted}
-				{...rest}
-			/>
-		</Suspense>
-	);
+  return (
+    // restricted = false meaning public route
+    // restricted = true meaning restricted route
+    <Suspense fallback={<ZFallbackIonSpinner />}>
+      <PublicRouteAsync
+        Component={Component}
+        restricted={restricted}
+        {...rest}
+      />
+    </Suspense>
+  );
 };
 const PublicRouteAsync = ({ Component, restricted, ...rest }: any) => {
-	const loggedIn = useRecoilValue(IsAuthenticatedRStateSelector);
+  const loggedIn = useRecoilValue(IsAuthenticatedRStateSelector);
 
-	return (
-		// restricted = false meaning public route
-		// restricted = true meaning restricted route
-		<Route
-			{...rest}
-			render={(props) =>
-				loggedIn && restricted ? (
-					<Redirect to={ZRoutesRedirects.AUTHENTICATED_USER_REDIRECT} />
-				) : (
-					<Component {...props} />
-				)
-			}
-		/>
-	);
+  return (
+    // restricted = false meaning public route
+    // restricted = true meaning restricted route
+    <Route
+      {...rest}
+      render={props =>
+        loggedIn && restricted ? (
+          <Redirect to={ZRoutesRedirects.AUTHENTICATED_USER_REDIRECT} />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
 };
 
 export default PublicRoute;
